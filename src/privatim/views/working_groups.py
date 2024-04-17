78b04399a657302e76e683b4ae2295da656c5a89
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2024-04-17 20:53:44 +0200

    Actually display the working group

--- src/privatim/views/working_groups.py
+++ src/privatim/views/working_groups.py
@@ -1,21 +1,16 @@
-from typing import TYPE_CHECKING
-
 from sqlalchemy import asc, select
-
 from privatim.models import Group
 
 
+from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from pyramid.interfaces import IRequest
     from privatim.types import RenderData
 
 
 def group_view(request: 'IRequest') -> 'RenderData':
-    # query all the working groups
-
     session = request.dbsession
-
     q = select(Group).order_by(asc(Group.name))
-    groups = session.execute(q).all()
+    groups = session.execute(q).unique().scalars().all()
 
     return {'groups': groups}
