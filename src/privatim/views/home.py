3bc597a15ac14cee4fd90b3c42e5252c5ff59075
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2024-05-06 16:02:38 +0200

    Add comment for clarification of home view

--- src/privatim/views/home.py
+++ src/privatim/views/home.py
@@ -7,6 +7,7 @@ if TYPE_CHECKING:
 
 
 def home_view(request: 'IRequest') -> HTTPFound:
+    """ The home view is the view that is called after the user logs in."""
     if request.authenticated_userid:
         url = request.route_url('activities')  # default page redirect (
         # temporary)
