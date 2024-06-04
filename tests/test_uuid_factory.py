9ea2e8bfab931f4a4bc82aba3eb16c9f90638010
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2024-06-04 16:52:12 +0200

    fixes fest

--- tests/test_uuid_factory.py
+++ tests/test_uuid_factory.py
@@ -3,7 +3,6 @@ import pytest
 from privatim.route_factories import create_uuid_factory
 from pyramid.testing import DummyRequest
 from pyramid.httpexceptions import HTTPNotFound
-from test_cache import DummyObject
 from tests.shared.utils import create_meeting
 
 
@@ -14,7 +13,8 @@ def test_create_uuid_factory_valid_uuid(session):
     db.flush()
     db.refresh(meeting)
 
-    request = DummyRequest()
+    request = DummyRequest(dbsession=session)
+
     request.matchdict = {'id': str(meeting.id)}
     route_factory = create_uuid_factory(Meeting)
     result = route_factory(request)
@@ -22,7 +22,7 @@ def test_create_uuid_factory_valid_uuid(session):
     assert isinstance(result, Meeting)
     assert result.id == meeting.id
 
-    request = DummyRequest()
+    request = DummyRequest(dbsession=session)
     request.matchdict = {'id': 'invalid-uuid'}
 
     with pytest.raises(HTTPNotFound):
@@ -30,10 +30,13 @@ def test_create_uuid_factory_valid_uuid(session):
 
 
 def test_create_uuid_factory_missing_uuid(session):
-    request = DummyRequest()
+    request = DummyRequest(dbsession=session)
     request.matchdict = {}
 
-    route_factory = create_uuid_factory(DummyObject())
+    class TestModel:
+        pass
+
+    route_factory = create_uuid_factory(TestModel)
     with pytest.raises(HTTPNotFound):
         route_factory(request)
 
@@ -42,7 +45,7 @@ def test_create_uuid_factory_not_found(session):
     # Arrange
     create_meeting()
 
-    request = DummyRequest()
+    request = DummyRequest(dbsession=session)
     request.matchdict = {'id': 'a1b2c3d4-e5f6-g7h8-i9j0-k1l2m3n4o5p6'}
 
     route_factory = create_uuid_factory(Meeting)
