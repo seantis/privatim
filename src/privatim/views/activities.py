cb28dcf62f169dd4b73ffee7eb7c42df10d1b152
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2024-06-11 01:06:54 +0200

    simplify the landing page query

--- src/privatim/views/activities.py
+++ src/privatim/views/activities.py
@@ -1,61 +1,44 @@
-from sqlalchemy import select, desc, union_all, literal_column
+from sqlalchemy import select, union_all, desc, cast, String, literal
 from privatim.models import Consultation, Meeting
 from privatim.i18n import _
 
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from pyramid.interfaces import IRequest
     from ..types import RenderData
-    from sqlalchemy import Select
-    from datetime import datetime
 
 
 def activities_view(request: 'IRequest') -> 'RenderData':
     """ Display all activities in the system. (It's the landing page.)"""
 
     session = request.dbsession
-    consultation_stmt: 'Select[tuple[str, datetime, Any]]' = select(
+    consultation_stmt = select(
         Consultation.id.label('id'),
         Consultation.created.label('timestamp'),
-        literal_column("'consultation'").label('type')
+        cast(literal("'consultation'"), String).label('type')
     )
-
-    meeting_stmt: 'Select[Any]' = select(
+    meeting_stmt = select(
         Meeting.id.label('id'),
         Meeting.time.label('timestamp'),
-        literal_column("'meeting'").label('type')
+        cast(literal("'meeting'"), String).label('type')
     )
-
     union_stmt = union_all(consultation_stmt, meeting_stmt)
+
+    # sort all
     ordered_union_stmt = select(
         union_stmt.c.id, union_stmt.c.timestamp, union_stmt.c.type
     ).order_by(desc('timestamp'))
-
     result = session.execute(ordered_union_stmt).fetchall()
-    consultation_ids = [row.id for row in result if row.type == 'consultation']
-    meeting_ids = [row.id for row in result if row.type == 'meeting']
 
-    consultations = session.scalars(
-        select(Consultation).filter(Consultation.id.in_(consultation_ids))
-    ).all()
-
-    meetings = session.scalars(
-        select(Meeting).filter(Meeting.id.in_(meeting_ids))
-    ).all()
-
-    # Create a dictionary for quick lookup
-    consultation_dict = {
-        consultation.id: consultation for consultation in consultations
-    }
-    meeting_dict = {meeting.id: meeting for meeting in meetings}
-
-    activities: list[Consultation | Meeting] = []
-    for row in result:
-        if row.type == 'consultation':
-            activities.append(consultation_dict[row.id])
-        elif row.type == 'meeting':
-            activities.append(meeting_dict[row.id])
+    activities = [
+        (
+            session.get(Consultation, row.id)
+            if row.type == "'consultation'"
+            else session.get(Meeting, row.id)
+        )
+        for row in result
+    ]
 
     return {
         'activities': activities,
