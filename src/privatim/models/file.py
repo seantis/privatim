a9934767ffcc787e07e3da5070c4f473fc1aee55
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2024-06-23 06:22:34 +0200

    fix test by removing unnecessary assert.

--- src/privatim/models/file.py
+++ src/privatim/models/file.py
@@ -30,7 +30,6 @@ class GeneralFile(Base, Associable):
     filename: Mapped[str] = mapped_column(nullable=False)
 
     def __init__(self, filename: str, content: bytes) -> None:
-        assert isinstance(content, bytes)
         self.id = str(uuid.uuid4())
         self.filename = filename
         self.file = File(content=content, filename=filename)
