42c502039594268ba045919654d4fb725ed563ab
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2024-07-11 01:11:55 +0200

    Add logging

--- src/privatim/file/setup.py
+++ src/privatim/file/setup.py
@@ -1,11 +1,14 @@
 """
 Setup static file storage.
 """
+import logging
 from pathlib import Path
 from typing import Any
 
 from libcloud.storage.drivers.local import LocalStorageDriver
 from sqlalchemy_file.storage import StorageManager
+
+log = logging.getLogger(__name__)
 
 
 def setup_filestorage(settings: Any) -> None:
@@ -13,6 +16,8 @@ def setup_filestorage(settings: Any) -> None:
     Configure storage of static assets
     """
     documents_dir = Path(settings.get('documents_dir', 'uploads'))
+    log.error(f'documents_dir: {documents_dir}')
+
     asset_dir = documents_dir / 'assets'
     asset_dir.mkdir(exist_ok=True, parents=True)
 
