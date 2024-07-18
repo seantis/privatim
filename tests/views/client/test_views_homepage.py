da3c1fbc651466a9e4aa3bc6657dd8991200a94e
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2024-07-18 15:18:20 +0200

    Fix translation for Search and add test (SEA-1390)

--- tests/views/client/test_views_homepage.py
+++ tests/views/client/test_views_homepage.py
@@ -1,6 +1,9 @@
-def test_filter(client):
+
+def test_translation_navbar(client):
     client.login_admin()
 
     page = client.get('/activities')
-    form = page.forms['filter_activities']
-    breakpoint()
+    form = page.pyquery('form#search')[0]
+    input_search = form[1]
+    assert input_search.get('placeholder') == 'Suchen...'
+
