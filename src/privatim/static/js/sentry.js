4457b66b6789308a3b3f4c844073e0ef603a3c51
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2024-08-30 13:43:06 +0200

    Fix typo in dsn [skip ci]

--- src/privatim/static/js/sentry.js
+++ src/privatim/static/js/sentry.js
@@ -1,2 +1,2 @@
-var sentry_dsn = document.documentElement.getAttribute('data-sentry_dsn');
+var sentry_dsn = document.documentElement.getAttribute('data-sentry-dsn');
 Sentry.init({dsn: sentry_dsn});
