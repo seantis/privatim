acb668b15806f2c8e75903e101b7ed0effa2126a
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2024-09-07 23:04:32 +0200

    Fix `AttributeError: 'tuple' object has no attribute 'get'`

--- src/privatim/views/people.py
+++ src/privatim/views/people.py
@@ -146,7 +146,7 @@ def add_user_view(request: 'IRequest') -> 'RenderDataOrRedirect':
             mapping={
                 'first_name': user.first_name,
                 'last_name': user.last_name,
-            },
+            }
         )
 
         request.messages.add(message, 'success')
