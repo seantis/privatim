8e2d512e6183711a9ce2ca885963511c44340374
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-03-31 19:42:05 +0200

    fix: Remove comment field usage from homepage filter test

--- tests/views/client/test_views_homepage.py
+++ tests/views/client/test_views_homepage.py
@@ -35,38 +35,8 @@ def test_filter(client):
     form = page.forms['filter_activities']
     form['consultation'] = True
     form['meeting'] = True
-    form['comment'] = False
     form.submit().follow()
     page = client.get('/activities')
-    assert 'Powerpoint Parade' in page
-
-    # Test comment filter
-
-    page = client.get(f'/consultation/{cons.id}')
-    page.form['content'] = 'What an interesting thought'
-    page.form.submit()
-    page = client.get('/activities')
-    assert 'What an interesting thought' in page
-
-    form['consultation'] = True
-    form['meeting'] = True
-    form['comment'] = False
-    page = form.submit().follow()
-    assert 'What an interesting thought' not in page
-    assert 'Test Consultation' in page
-    assert '2nd Consultation' in page
-    assert 'Powerpoint Parade' in page
-
-    # Test all filters enabled
-    page = client.get('/activities')
-    form = page.forms['filter_activities']
-    form['consultation'] = True
-    form['meeting'] = True
-    form['comment'] = True
-    page = form.submit().follow()
-    assert 'What an interesting thought' in page
-    assert 'Test Consultation' in page
-    assert '2nd Consultation' in page
     assert 'Powerpoint Parade' in page
 
 
